0c2148e2328f704478c13d0fd606b93e41e430fe
Nikko Miu <nikkoamiu@gmail.com>
2019-04-29 12:13:40 -0500

    Make model public

--- jres.go
+++ jres.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-type resModel struct {
+type Model struct {
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 	Info    interface{} `json:"info"`
@@ -26,11 +26,11 @@ func Send(w http.ResponseWriter, status int, data interface{}) error {
 }
 
 func Accepted(w http.ResponseWriter, data interface{}) error {
-	return Send(w, http.StatusAccepted, resModel{Data: data})
+	return Send(w, http.StatusAccepted, Model{Data: data})
 }
 
 func BadRequest(w http.ResponseWriter, msg string) error {
-	res := resModel{Message: "bad request"}
+	res := Model{Message: "bad request"}
 	if msg == "" {
 		res.Message = msg
 	}
@@ -39,13 +39,13 @@ func BadRequest(w http.ResponseWriter, msg string) error {
 }
 
 func Conflict(w http.ResponseWriter, errors []string) error {
-	return Send(w, http.StatusConflict, resModel{Message: "conflict", Errors: errors})
+	return Send(w, http.StatusConflict, Model{Message: "conflict", Errors: errors})
 }
 
 func Created(w http.ResponseWriter, at string, data interface{}) error {
 	w.Header().Add("Location", at)
 
-	return Send(w, http.StatusCreated, resModel{Message: "entity created", Data: data})
+	return Send(w, http.StatusCreated, Model{Message: "entity created", Data: data})
 }
 
 func NoContent(w http.ResponseWriter) error {
@@ -53,7 +53,7 @@ func NoContent(w http.ResponseWriter) error {
 }
 
 func NotFound(w http.ResponseWriter, msg string) error {
-	data := resModel{Message: "record not found"}
+	data := Model{Message: "record not found"}
 	if msg == "" {
 		data.Message = msg
 	}
@@ -62,15 +62,15 @@ func NotFound(w http.ResponseWriter, msg string) error {
 }
 
 func Forbidden(w http.ResponseWriter) error {
-	return Send(w, http.StatusForbidden, resModel{Message: "forbidden"})
+	return Send(w, http.StatusForbidden, Model{Message: "forbidden"})
 }
 
 func MethodNotAllwed(w http.ResponseWriter, errors []string) error {
-	return Send(w, http.StatusMethodNotAllowed, resModel{Message: "method not allowed", Errors: errors})
+	return Send(w, http.StatusMethodNotAllowed, Model{Message: "method not allowed", Errors: errors})
 }
 
 func OK(w http.ResponseWriter, data interface{}) error {
-	return Send(w, http.StatusOK, resModel{Data: data})
+	return Send(w, http.StatusOK, Model{Data: data})
 }
 
 func Redirect(w http.ResponseWriter, to string) error {
@@ -80,17 +80,17 @@ func Redirect(w http.ResponseWriter, to string) error {
 }
 
 func ServerError(w http.ResponseWriter) error {
-	return Send(w, http.StatusInternalServerError, resModel{Message: "internal server error"})
+	return Send(w, http.StatusInternalServerError, Model{Message: "internal server error"})
 }
 
 func Unauthorized(w http.ResponseWriter, data interface{}) error {
-	return Send(w, http.StatusUnauthorized, resModel{Message: "unauthorized", Data: data})
+	return Send(w, http.StatusUnauthorized, Model{Message: "unauthorized", Data: data})
 }
 
 func UnprocessableEntity(w http.ResponseWriter, data interface{}) error {
-	return Send(w, http.StatusUnprocessableEntity, resModel{Message: "unprocessable entity", Data: data})
+	return Send(w, http.StatusUnprocessableEntity, Model{Message: "unprocessable entity", Data: data})
 }
 
 func ValidationError(w http.ResponseWriter, errors []string) error {
-	return Send(w, http.StatusBadRequest, resModel{Message: "validation error", Errors: errors})
+	return Send(w, http.StatusBadRequest, Model{Message: "validation error", Errors: errors})
 }
